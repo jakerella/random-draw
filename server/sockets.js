5e7c64e12a830a45b4c4ec6eec0a092792854578
jakerella <jordan@jordankasper.com>
2015-03-19 10:49:31 -0400

    More function break out; added 'entrants' room

--- server/sockets.js
+++ server/sockets.js
@@ -3,28 +3,47 @@ var connections = {};
 
 module.exports = function setupSockets(io) {
 
-    io.on('connection', function(socket) {
-      console.log('User ' + socket.id + ' connected...');
-
-      connections[socket.id] = {
-        socket: socket,
-        name: null
-      };
-
-      socket.on('disconnect', function() {
-        console.log('...user ' + socket.id + ' disconnected.');
-
-        delete connections[socket.id];
-      });
-
-      socket.on('chooseme', function(name) {
-        console.log(socket.id + ' entered the drawing as ' + name);
-
-        if (connections[socket.id]) {
-          connections[socket.id].name = name;
-          socket.emit('entered');
-        }
-      });
-    });
+    io.on('connection', connect);
 
 };
+
+function connect(socket) {
+    console.log('User ' + socket.id + ' connected...');
+
+    connections[socket.id] = {
+        socket: socket,
+        name: null
+    };
+
+    socket.on('disconnect', disconnect);
+    socket.on('enter', enterDrawing);
+}
+
+function disconnect() {
+    var socket = this;
+
+    console.log(socket.id + ' disconnected.');
+
+    if (connections[socket.id]) {
+        delete connections[socket.id];
+    }
+}
+
+function enterDrawing(name) {
+    var socket = this;
+
+    socket.join('entrants', function(err) {
+        if (err) {
+            console.log('Unable to add ' + this.id + ' to /entrants!');
+            return console.error(err);
+        }
+
+        if (connections[socket.id]) {
+            connections[socket.id].name = name;
+            console.log(socket.id + ' entered the drawing as ' + name);
+            socket.emit('entered');
+        } else {
+            console.error('Cannot find' + socket.id + ' in the connections!');
+        }
+    });
+}
